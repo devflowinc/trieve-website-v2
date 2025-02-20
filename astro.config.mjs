3ad864c78ab790d2f8f285a4382ec3e3fe5d9b3f
Jan Kraus <kraus.jan@gmail.com>
2025-02-20 18:22:18 +0000

    feat: add support for sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,21 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
 import keystatic from "@keystatic/astro";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://trieve.ai/",
   devToolbar: {
     enabled: false,
   },
   image: {
     domains: ["127.0.0.1"],
   },
-  integrations: [react(), markdoc(), keystatic()],
+  integrations: [react(), markdoc(), keystatic(), sitemap()],
   vite: {
     resolve: {
       alias: {
